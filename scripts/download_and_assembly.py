afe6509cd1461233e9b80348e31782c5267c0201
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-06-16 20:03:18 +0100

    Adding base scripts

---
+++ scripts/download_and_assembly.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import requests
+import hashlib
+import argparse
+import os
+import subprocess
+
+# Initialize the parser
+parser = argparse.ArgumentParser(description="This script downloads files from provided urls, checks their integrity with md5 hashes and then assembles them with Shovill.")
+
+# Add the arguments
+parser.add_argument('input_file', metavar='input', type=str, help='the input TSV file path')
+
+# Parse the arguments
+args = parser.parse_args()
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # Ensure we got an OK response
+
+    with open(filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+def compute_md5(filename):
+    hash_md5 = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+# Read the TSV file
+df = pd.read_csv(args.input_file, delimiter='\t')
+
+for _, row in df.iterrows():
+    filenames = []
+    for i in range(1, 3):  # For both files R1 and R2
+        url = row['fastq_ftp_R' + str(i)]
+        md5_expected = row['fastq_md5_R' + str(i)]
+        
+        filename = os.path.basename(url)
+
+        # Download the file
+        download_file(url, filename)
+
+        # Compute the file's md5 hash and compare it with the expected value
+        md5_actual = compute_md5(filename)
+        
+        if md5_actual != md5_expected:
+            raise ValueError(f"File {filename} md5 check failed. Expected {md5_expected}, got {md5_actual}")
+        else:
+            print(f"File {filename} downloaded and md5 check passed.")
+            
+        filenames.append(filename)
+    
+    # Make output dir
+    outdir = row['run_accession']
+    os.makedirs(outdir, exist_ok=True)
+    
+    # Assemble the reads with Shovill
+    subprocess.run(['shovill', '--R1', filenames[0], '--R2', filenames[1], '--outdir', outdir, '--cpus', '1'], check=True)
+    
+    # Delete the input files
+    for filename in filenames:
+        os.remove(filename)
+
---
+++ scripts/remove_samples_that_are_not_paired.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import argparse
+
+# Initialize the parser
+parser = argparse.ArgumentParser(description="This script filters a TSV file and splits a specific column.")
+
+# Add the arguments
+parser.add_argument('input_file', metavar='input', type=str, help='the input TSV file path')
+parser.add_argument('output_file', metavar='output', type=str, help='the output TSV file path')
+parser.add_argument('error_file', metavar='error', type=str, help='the error TSV file path')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Read the TSV file
+df = pd.read_csv(args.input_file, delimiter='\t')
+
+# Keep only desired columns
+df = df[['run_accession', 'fastq_ftp', 'fastq_md5', 'read_count', 'base_count']]
+
+# Split fastq_ftp column and expand into new dataframe
+split_df = df['fastq_ftp'].str.split(';', expand=True)
+
+# Create error dataframe
+error_df = df[split_df.iloc[:, :2].isna().any(axis=1) & ~split_df.iloc[:, 2:].isna().all(axis=1)]
+error_df.to_csv(args.error_file, sep='\t', index=False)
+
+# Filter df for rows that are not in error_df
+df = df[~df['run_accession'].isin(error_df['run_accession'])]
+
+# Add split columns back to the original dataframe
+df[['fastq_ftp_R1', 'fastq_ftp_R2']] = split_df.loc[df.index, :].iloc[:, 0:2]
+
+# Split fastq_md5 column and expand into new dataframe
+split_md5_df = df['fastq_md5'].str.split(';', expand=True)
+df[['fastq_md5_R1', 'fastq_md5_R2']] = split_md5_df.loc[df.index, :].iloc[:, 0:2]
+
+# Remove the original 'fastq_ftp' and 'fastq_md5' columns
+df = df.drop(columns=['fastq_ftp', 'fastq_md5'])
+
+# Cast read_count and base_count to int
+df['read_count'] = df['read_count'].astype(int)
+df['base_count'] = df['base_count'].astype(int)
+
+# Write the result back to a new TSV file
+df.to_csv(args.output_file, sep='\t', index=False)
+
