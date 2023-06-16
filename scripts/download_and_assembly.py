a6e2353149f5a8ec19ebf791dfe662c89f4f77cc
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-06-16 21:14:01 +0100

    Updating download_and_assembly

--- scripts/download_and_assembly.py
+++ scripts/download_and_assembly.py
@@ -6,10 +6,11 @@ import os
 import subprocess
 
 # Initialize the parser
-parser = argparse.ArgumentParser(description="This script downloads files from provided urls, checks their integrity with md5 hashes and then assembles them with Shovill.")
+parser = argparse.ArgumentParser(description="This script downloads files from provided URLs, checks their integrity with MD5 hashes, and then assembles them with Shovill.")
 
 # Add the arguments
 parser.add_argument('input_file', metavar='input', type=str, help='the input TSV file path')
+parser.add_argument('output', metavar='output', type=str, help='the output directory path')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -33,35 +34,37 @@ def compute_md5(filename):
 # Read the TSV file
 df = pd.read_csv(args.input_file, delimiter='\t')
 
+# Create the output directory if it doesn't exist
+os.makedirs(args.output)
+
 for _, row in df.iterrows():
     filenames = []
     for i in range(1, 3):  # For both files R1 and R2
         url = row['fastq_ftp_R' + str(i)]
         md5_expected = row['fastq_md5_R' + str(i)]
-        
-        filename = os.path.basename(url)
+
+        filename = os.path.join(args.output, os.path.basename(url))
 
         # Download the file
         download_file(url, filename)
 
-        # Compute the file's md5 hash and compare it with the expected value
+        # Compute the file's MD5 hash and compare it with the expected value
         md5_actual = compute_md5(filename)
-        
+
         if md5_actual != md5_expected:
-            raise ValueError(f"File {filename} md5 check failed. Expected {md5_expected}, got {md5_actual}")
+            raise ValueError(f"File {filename} MD5 check failed. Expected {md5_expected}, got {md5_actual}")
         else:
-            print(f"File {filename} downloaded and md5 check passed.")
-            
+            print(f"File {filename} downloaded and MD5 check passed.")
+
         filenames.append(filename)
-    
-    # Make output dir
-    outdir = row['run_accession']
+
+    # Make output directory within the specified output directory
+    outdir = os.path.join(args.output, row['run_accession'])
     os.makedirs(outdir, exist_ok=True)
-    
+
     # Assemble the reads with Shovill
     subprocess.run(['shovill', '--R1', filenames[0], '--R2', filenames[1], '--outdir', outdir, '--cpus', '1'], check=True)
-    
+
     # Delete the input files
     for filename in filenames:
         os.remove(filename)
-
