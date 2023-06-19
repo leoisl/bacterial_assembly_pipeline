fb479bcddb06920ee44d3817089f7b2c3b309c45
Leandro Lima <leandro@hl-codon-04-01.ebi.ac.uk>
2023-06-20 00:23:06 +0100

    Updating submit_lsf.sh

--- submit_lsf.sh
+++ submit_lsf.sh
@@ -2,17 +2,18 @@
 set -eux
 
 MEMORY=20000
-PROFILE="lsf"
+PROFILE="lsf_bacterial_assembly_pipeline"
 LOG_DIR=logs/
 JOB_NAME="snakemake_bacterial_assembly_pipeline."$(date --iso-8601='minutes')
 
 mkdir -p $LOG_DIR
 
 bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
-    -M "$MEMORY" \
-    -o "$LOG_DIR"/"$JOB_NAME".o \
-    -e "$LOG_DIR"/"$JOB_NAME".e \
-    -J "$JOB_NAME" \
-      snakemake --profile "$PROFILE" --keep-going --restart-times 0 "$@"
+     -q long \
+     -M "$MEMORY" \
+     -o "$LOG_DIR"/"$JOB_NAME".o \
+     -e "$LOG_DIR"/"$JOB_NAME".e \
+     -J "$JOB_NAME" \
+     snakemake --profile "$PROFILE" --keep-going "$@"
 
 exit 0
