e6eca7b20286633f45083cf30e2e237809e0ab8b
Leandro Lima <leandro@hl-codon-50-04.ebi.ac.uk>
2023-06-19 15:34:29 +0100

    Adding script scripts/list_assembled_genomes.sh

---
+++ scripts/list_assembled_genomes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if a directory has been provided
+if [ $# -eq 0 ]; then
+    echo "No directory supplied. Usage: ./script.sh /path/to/tar/files"
+    exit 1
+fi
+
+# Directory to search for tar.gz files
+dir=$1
+
+for file in "$dir"/*.tar.gz
+do
+    echo "Processing $file"
+    tar -tf $file | grep 'contigs.fa'
+done
+
