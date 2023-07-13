60bb2a31002f06f1ec8c8ffa7fc2af1332fbf3dd
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-07-13 16:01:32 +0100

    Adding a flag to ensure the copy was done correctly

--- scripts/utils/copy_large_assembly_dir/copy_and_verify.sh
+++ scripts/utils/copy_large_assembly_dir/copy_and_verify.sh
@@ -4,7 +4,7 @@ function copy_and_check {
     input_file=$1
     output_file=$2
 
-    rsync -av $input_file $output_file
+    rsync -aq $input_file $output_file
 
     orig_checksum=$(md5sum $input_file | awk '{print $1}')
     copied_checksum=$(md5sum $output_file | awk '{print $1}')
@@ -17,3 +17,4 @@ function copy_and_check {
 
 copy_and_check "${snakemake_input[input_assembly_file]}" "${snakemake_output[output_assembly_file]}"
 copy_and_check "${snakemake_input[input_metadata_file]}" "${snakemake_output[output_metadata_file]}"
+touch "${snakemake_output[flag]}"
