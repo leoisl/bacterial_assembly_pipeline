74e8406f9861172d6d54f5f39c716a2f7066b53b
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-07-17 10:30:11 +0100

    Updating restart-times

--- submit_lsf.sh
+++ submit_lsf.sh
@@ -14,6 +14,6 @@ bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
      -o "$LOG_DIR"/"$JOB_NAME".o \
      -e "$LOG_DIR"/"$JOB_NAME".e \
      -J "$JOB_NAME" \
-     snakemake --profile "$PROFILE" --keep-going --restart-times 0 --rerun-trigger mtime "$@"
+     snakemake --profile "$PROFILE" --keep-going --restart-times 1 --rerun-trigger mtime "$@"
 
 exit 0
