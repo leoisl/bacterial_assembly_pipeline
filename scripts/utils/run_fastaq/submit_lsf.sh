6908b8200fd3959ae5338b7c7c1b26c9874e45f7
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-08-01 15:07:36 +0100

    Adding run_fastaq pipeline

---
+++ scripts/utils/run_fastaq/submit_lsf.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eux
+
+MEMORY=1000
+PROFILE="lsf_bacterial_assembly_pipeline"
+LOG_DIR=logs/
+JOB_NAME="snakemake_run_fastaq_pipeline."$(date --iso-8601='minutes')
+
+mkdir -p $LOG_DIR
+
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+     -M "$MEMORY" \
+     -o "$LOG_DIR"/"$JOB_NAME".o \
+     -e "$LOG_DIR"/"$JOB_NAME".e \
+     -J "$JOB_NAME" \
+     snakemake --profile "$PROFILE" --keep-going --restart-times 0 "$@"
+
+exit 0
