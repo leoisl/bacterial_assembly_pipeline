23de0db741c3a55aa51077541a8b60428d80278a
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-08-15 09:47:04 +0100

    Updating Path in rename_md5sums

--- scripts/release_scripts/rename_md5sums.py
+++ scripts/release_scripts/rename_md5sums.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import os
 
 # Define the CLI argument parser
 parser = argparse.ArgumentParser(description='Process a TSV file.')
@@ -8,9 +9,14 @@ parser.add_argument('outfile', type=str, help='Path to save the processed output
 args = parser.parse_args()
 
 # Read the data into a dataframe
-df = pd.read_csv(args.infile, sep="\t", header=None)
+df = pd.read_csv(args.infile, sep="\t", header=None, names=["Sample", "Path"])
 
-df[1] = df[1].apply(lambda x: "ilmn-" + x)
+def get_updated_path(path):
+    old_filename = os.path.basename(path).strip()
+    new_filename = 'ilmn-' + old_filename
+    new_path = os.path.join(os.path.dirname(path), new_filename)
+    return new_path
+df["Path"] = df["Path"].apply(get_updated_path)
 
 # Write the dataframe to the output file path specified in the arguments
 df.to_csv(args.outfile, sep="\t", index=False, header=False)
